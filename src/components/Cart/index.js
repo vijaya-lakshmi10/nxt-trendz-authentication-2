132c6302b5bce35012d032ea9cdc46f2357dbd06
vijayalakshmi <vijayalakshmilanka997@gmail.com>
2023-09-02 06:45:53 +0000

    Nxt Trendz Authentication Part 2 Using React JS

---
+++ src/components/Cart/index.js
@@ -0,0 +1,12 @@
+// Write your JS code here
+import './index.css'
+import Header from '../Header'
+const Cart=()=>(
+    <>
+    <Header/>
+    <div className="cart-container">
+    <img src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png" alt="cart" className="cart-img"/>
+    </div>
+    </>
+)
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,12 @@
+// Write your JS code here
+import './index.css'
+import Header from '../Header'
+const Products=()=>(
+    <>
+    <Header/>
+    <div className="products-container">
+    <img src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png" alt="products" className="products-img"/>
+    </div>
+    </>
+)
+export default Products
